e0d8a11ab8deed2b83a1c67277171f4f1e5ff00c
Ramghh <137633823+Ramghh@users.noreply.github.com>
2026-07-07 03:10:17 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ⚠️ لازم نزيدو CACHE_NAME (نبدلو الرقم) كل مرة نبدلو فيها index.html أو أي ملف من app-shell
 // هذا يجبر الأجهزة تجيب النسخة الجديدة بدل ما تخدم بالقديمة المخزنة (cache).
-const CACHE_NAME = 'tabrid-cache-v8';
+const CACHE_NAME = 'tabrid-cache-v10';
 
 const APP_SHELL = [
   './',
